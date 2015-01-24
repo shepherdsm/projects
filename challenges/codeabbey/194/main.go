6c2b7b8433b6f0b87a5fb5fb60293ac332b513a3
Scott Shepherd <scottmshepherd@gmail.com>
2015-01-24 11:20:20 -0500

    Solved #194 Convex Hull and Farmers.

--- challenges/codeabbey/194/main.go
+++ challenges/codeabbey/194/main.go
@@ -13,8 +13,57 @@ var gStartPost *Post
 // Convex Hull and Farmers
 // http://www.codeabbey.com/index/task_view/convex-hull-and-farmers
 func main() {
-	posts := getPosts()
-	sort.Sort(posts)
+	posts := getConvexHull()
+
+	posts.printPosts()
+}
+
+// Ugliest printing method ever.
+func (p Posts) printPosts() {
+	hull := make(Posts, 0, len(p))
+	end := p[0]
+	hull = append(hull, gStartPost)
+
+	// Pull out the points that define the convex hull.
+	for _, val := range p[1:] {
+		if end.ind == val.ind {
+			break
+		}
+
+		hull = append(hull, val)
+	}
+
+	hull = append(hull, end)
+
+	var (
+		start   int
+		largest int
+	)
+
+	// Find the largest y value to be our start.
+	for ind, val := range hull {
+		if val.p.y > largest {
+			largest = val.p.y
+			start = ind
+		}
+	}
+
+	index := start
+
+	// We'll print in clockwise order, but because of how we choose
+	// our starting point for the Graham Scan algorithm, we may be in
+	// the middle of the array.
+	for {
+		fmt.Printf("%d ", hull[index].ind)
+		index--
+
+		if index == -1 {
+			index = len(hull) - 1
+		}
+		if index == start {
+			break
+		}
+	}
 }
 
 // Loads all of the posts into an array and sets the global gStartPost
@@ -53,6 +102,34 @@ func getPosts() Posts {
 	return posts
 }
 
+// Runs the Graham Scan algorithm to find the Convex Hull for the set of points.
+func getConvexHull() Posts {
+	posts := getPosts()
+	sort.Sort(posts)
+	totalPoints := len(posts)
+
+	posts[0] = posts[totalPoints-1]
+
+	numPoints := 1
+
+	for i := 2; i < totalPoints; i++ {
+		for isLeftTurn(posts[numPoints-1], posts[numPoints], posts[i]) <= 0 {
+			if numPoints > 1 {
+				numPoints -= 1
+			} else if i == totalPoints {
+				break
+			} else {
+				i++
+			}
+		}
+
+		numPoints++
+		posts[numPoints], posts[i] = posts[i], posts[numPoints]
+	}
+
+	return posts
+}
+
 type Posts []*Post
 
 type Post struct {
@@ -63,6 +140,13 @@ type Post struct {
 type Point struct {
 	x int
 	y int
+}
+
+// Determines if moving from p1 -> p2 -> p3 consitutes a left turn or right turn.
+func isLeftTurn(p1, p2, p3 *Post) int {
+	turn := (p2.p.x-p1.p.x)*(p3.p.y-p1.p.y) - (p2.p.y-p1.p.y)*(p3.p.x-p1.p.x)
+
+	return turn
 }
 
 // Used to determine the starting point.
@@ -79,7 +163,7 @@ func (p1 *Post) lessAngle(p2 *Post) bool {
 	return angle1 < angle2
 }
 
-// Determines the angle between the current post and the global startPost.
+// Determines the angle between the x-axis and the line from gStartPoint -> p1
 func (p1 *Post) angle() float64 {
 	deltaY := float64(p1.p.y - gStartPost.p.y)
 	deltaX := float64(p1.p.x - gStartPost.p.x)
