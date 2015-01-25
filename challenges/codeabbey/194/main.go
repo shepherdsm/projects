b19f45fed3ac858587f872e5a9746f8e901e68c3
Scott Shepherd <scottmshepherd@gmail.com>
2015-01-25 09:36:28 -0500

    Tidied up the solution to #194.

--- challenges/codeabbey/194/main.go
+++ challenges/codeabbey/194/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"bufio"
+	"errors"
 	"fmt"
 	"math"
 	"os"
@@ -13,97 +13,44 @@ var gStartPost *Post
 // Convex Hull and Farmers
 // http://www.codeabbey.com/index/task_view/convex-hull-and-farmers
 func main() {
-	posts := getConvexHull()
+	posts, _ := getConvexHull()
 
-	posts.printPosts()
-}
-
-// Ugliest printing method ever.
-func (p Posts) printPosts() {
-	hull := make(Posts, 0, len(p))
-	end := p[0]
-	hull = append(hull, gStartPost)
-
-	// Pull out the points that define the convex hull.
-	for _, val := range p[1:] {
-		if end.ind == val.ind {
-			break
-		}
-
-		hull = append(hull, val)
-	}
-
-	hull = append(hull, end)
-
-	var (
-		start   int
-		largest int
-	)
-
-	// Find the largest y value to be our start.
-	for ind, val := range hull {
-		if val.p.y > largest {
-			largest = val.p.y
-			start = ind
-		}
-	}
-
-	index := start
-
-	// We'll print in clockwise order, but because of how we choose
-	// our starting point for the Graham Scan algorithm, we may be in
-	// the middle of the array.
-	for {
-		fmt.Printf("%d ", hull[index].ind)
-		index--
-
-		if index == -1 {
-			index = len(hull) - 1
-		}
-		if index == start {
-			break
-		}
+	for _, val := range posts {
+		fmt.Printf("%d ", val.ind)
 	}
 }
 
 // Loads all of the posts into an array and sets the global gStartPost
 func getPosts() Posts {
-	scanner := bufio.NewScanner(os.Stdin)
-
 	var totalPosts int
-
-	scanner.Scan()
-	fmt.Sscanf(scanner.Text(), "&d", &totalPosts)
+	fmt.Fscan(os.Stdin, &totalPosts)
 	posts := make(Posts, 0, totalPosts)
 
 	var (
-		currentPost int
-		x           int
-		y           int
+		x int
+		y int
 	)
 
-	for scanner.Scan() {
-		fmt.Sscanf(scanner.Text(), "%d %d", &x, &y)
-		curPost := &Post{&Point{x, y}, currentPost}
+	fmt.Fscan(os.Stdin, &x, &y)
+	gStartPost = &Post{&Point{x, y}, 0}
+	posts = append(posts, gStartPost)
+
+	for i := 1; i < totalPosts; i++ {
+		fmt.Fscan(os.Stdin, &x, &y)
+		curPost := &Post{&Point{x, y}, i}
 
 		posts = append(posts, curPost)
 
-		if gStartPost == nil {
+		if curPost.greater(gStartPost) {
 			gStartPost = curPost
 		}
-
-		if curPost.less(gStartPost) {
-			gStartPost = curPost
-		}
-
-		currentPost++
 	}
 
 	return posts
 }
 
 // Runs the Graham Scan algorithm to find the Convex Hull for the set of points.
-func getConvexHull() Posts {
+func getConvexHull() (Posts, error) {
 	posts := getPosts()
 	sort.Sort(posts)
 	totalPoints := len(posts)
@@ -113,7 +60,7 @@ func getConvexHull() Posts {
 	numPoints := 1
 
 	for i := 2; i < totalPoints; i++ {
-		for isLeftTurn(posts[numPoints-1], posts[numPoints], posts[i]) <= 0 {
+		for isLeftTurn(posts[numPoints-1], posts[numPoints], posts[i]) > 0 {
 			if numPoints > 1 {
 				numPoints -= 1
 			} else if i == totalPoints {
@@ -127,7 +74,14 @@ func getConvexHull() Posts {
 		posts[numPoints], posts[i] = posts[i], posts[numPoints]
 	}
 
-	return posts
+	for ind, val := range posts[1:] {
+		if posts[0].ind == val.ind {
+			posts[0] = gStartPost
+			return posts[:ind+2], nil
+		}
+	}
+
+	return nil, errors.New("No convex hull found.")
 }
 
 type Posts []*Post
@@ -150,17 +104,17 @@ func isLeftTurn(p1, p2, p3 *Post) int {
 }
 
 // Used to determine the starting point.
-func (p1 *Post) less(p2 *Post) bool {
-	return (p1.p.y < p2.p.y) ||
+func (p1 *Post) greater(p2 *Post) bool {
+	return (p1.p.y > p2.p.y) ||
 		(p1.p.y == p2.p.y && p1.p.x < p2.p.x)
 }
 
-// Returns if p1 has a smaller angle compared to the startPost than p2.
-func (p1 *Post) lessAngle(p2 *Post) bool {
+// Returns if p1 has a greater angle compared to the startPost than p2.
+func (p1 *Post) greaterAngle(p2 *Post) bool {
 	angle1 := p1.angle()
 	angle2 := p2.angle()
 
-	return angle1 < angle2
+	return angle1 > angle2
 }
 
 // Determines the angle between the x-axis and the line from gStartPoint -> p1
@@ -177,7 +131,7 @@ func (p Posts) Len() int {
 }
 
 func (p Posts) Less(i, j int) bool {
-	return p[i].lessAngle(p[j])
+	return p[i].greaterAngle(p[j])
 }
 
 func (p Posts) Swap(i, j int) {
